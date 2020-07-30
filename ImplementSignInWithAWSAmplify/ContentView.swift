48c9e6783110b10c2767aa37a55f0685074630d2
daviswww <ho861206@gmail.com>
2020-07-30 16:55:52 +0800

    Initial Commit

---
+++ ImplementSignInWithAWSAmplify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImplementSignInWithAWSAmplify
+//
+//  Created by 何星緯 on 2020/7/30.
+//  Copyright © 2020 davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
